0c3b1d735369309027ecbe34630bbc4e524c4886
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-03-12 06:14:38 +0530

    backend complete

--- Backend/contracts/ICryptoDevs.sol
+++ Backend/contracts/ICryptoDevs.sol
@@ -5,4 +5,5 @@ interface ICryptoDevs {
     
     function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256 tokenId);
     function balanceOf(address owner) external view returns (uint256 balance);
+    
 }
--- Backend/scripts/deploy.js
+++ Backend/scripts/deploy.js
@@ -1,32 +1,25 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+const { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS } = require("../constants");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
+  const cryptoDevsNFTContract = CRYPTO_DEVS_NFT_CONTRACT_ADDRESS;
+  const cryptDevsTokenContract = await ethers.getContractFactory(
+    "CryptoDevToken"
+  );
+  const deployedCryptoDevsTokenContract = await cryptDevsTokenContract.deploy(
+    cryptoDevsNFTContract
+  );
+  await deployedCryptoDevsTokenContract.deployed();
   console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    "Crypto Devs Token Contract Address:",
+    deployedCryptoDevsTokenContract.address
   );
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
